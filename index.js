8451ff708e4f0308c73baf68ed32f8f6b8aa71c0
JimmyStephen <jstephen@student.neumont.edu>
2021-12-09 21:03:17 -0700

    MyWebsite

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+    pug = require('pug'),
+    routes = require('./routes/routes');
+
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.get("/", routes.home);
+
+// app.get("/login", routes.logIn);
+// app.post("/login", urlEncodedParser, routes.logInAction);
+
+app.listen(process.env.PORT || 3000);
